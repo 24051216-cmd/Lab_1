d76c9becd0577654fc9ff412417e66cdbac79e2a
24051216-cmd <24051216@kiit.ac.in>
2026-02-12 17:40:37 +0530

    init commit

---
+++ src/StudentDetails.java
@@ -0,0 +1,9 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+
+        System.out.println("Name: Hercule Poirot");
+        System.out.println("Roll No: 24051216");
+        System.out.println("Section: CSE-07");
+        System.out.println("Branch: Computer Science");
+    }
+}
