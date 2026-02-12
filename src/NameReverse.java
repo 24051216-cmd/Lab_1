4fe83360660e6bc9ceebd32d0af931ff80377271
24051216-cmd <24051216@kiit.ac.in>
2026-02-12 17:47:20 +0530

    init commit

---
+++ src/NameReverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NameReverse {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first name: ");
+        String first = sc.nextLine();
+
+        System.out.print("Enter second name: ");
+        String last = sc.nextLine();
+
+        System.out.println(last + " " + first);
+    }
+}
